c680b88b5bf2719a19cc7ddd767bb4b90b32152f
jarda <jgresula@gmail.com>
2017-10-03 17:34:47 +0200

    fixed publishing to pypi

--- makefile
+++ makefile
@@ -4,9 +4,18 @@ dist:
 	@python setup.py clean && python setup.py sdist --formats=zip
 	@for fname in dist/* ; do mv $$fname "$${fname%.*}-python.$${fname##*.}" ; done
 
+# cat ~/.pypirc
+# [distutils]
+# index-servers =
+#  pypi
+# 
+# [pypi]
+# repository=https://upload.pypi.org/legacy/
+# username=$username-at-pypi
+# password=$password-at-pypi
 publish:
 	@rm -rf dist/* build/* python/MANIFEST
-	@python setup.py clean && python setup.py sdist upload
+	@python setup.py clean && python setup.py sdist upload -r pypi
 
 .PHONY: clean
 clean:
--- setup.py
+++ setup.py
@@ -44,15 +44,10 @@ The Pdfcrowd API lets you easily convert between HTML, PDF and various image for
                './image2pdf',
                './pdf2pdf'
                ],
-      classifiers=["Development Status :: 1 - Planning",
-                   "License :: OSI Approved :: MIT License",
+      classifiers=["License :: OSI Approved :: MIT License",
                    "Operating System :: MacOS",
                    "Operating System :: Microsoft",
                    "Operating System :: POSIX",
                    "Operating System :: Unix",
                    "Intended Audience :: Developers",
-                   "Programming Language :: Python :: 2.7'"
-                   "Programming Language :: Python :: 3",
-                   "Programming Language :: Python :: 3.4",
-                   "Programming Language :: Python :: 3.5",
                    "Topic :: Software Development :: Libraries"])
