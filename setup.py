daecdbfbd6b0e15fd4132a25e00d1bae641cd267
pavel <pavel@pdfcrowd.com>
2021-05-12 07:21:25 +0200

    minor doc update

--- setup.py
+++ setup.py
@@ -31,7 +31,7 @@ except ImportError:
 py_modules=['pdfcrowd']
 
 setup(name='pdfcrowd',
-      version='5.1.2',
+      version='5.1.3',
       description="A client library for the Pdfcrowd API.",
       url='https://pdfcrowd.com/doc/api/',
       license="License :: OSI Approved :: MIT License",
